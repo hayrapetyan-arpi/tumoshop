9880ce6d478369cb1a9c8041dd8ac7229a8df0ac
hayrapetyan-arpi <arpi.hayrapetyan.y@tumo.org>
2024-05-03 21:26:41 +0400

    first commit

---
+++ Category.php
@@ -0,0 +1,35 @@
+<?php
+
+    class Category {
+
+        public $id;
+        public $title;
+        public $image;
+
+        public static function create(){
+
+        }
+
+        public static function read(){
+            
+        }
+
+        public static function ubdate(){
+
+        }
+
+        public static function delete(){
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+?>
---
+++ Product.php
@@ -0,0 +1,32 @@
+<?php
+
+    class Product{
+
+        public $id;
+        public $title;
+        public $price;
+        public $image;
+        public $description;
+        public $reviews;
+        public $category;
+        public $status;
+
+
+        public static function create(){
+            
+        }
+
+        public static function read(){
+            
+        }
+
+        public static function ubdate(){
+
+        }
+
+        public static function delete(){
+
+        }
+    }
+
+?>
